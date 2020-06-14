10166ae17e16294a1b2e490c228d5f4bcfecc963
Bruce Parker <detailsharks@gmail.com>
2020-06-14 00:02:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,1 @@
 # code here!
-class School
-   attr_accessor :name, :roster, :grade
-   attr_reader :school 
-  
-  def initialize roster=(roster)
-    @roster = roster
-  end
-  
-  school.roster =[]
-school.add_student  
-
-
-  
-  
